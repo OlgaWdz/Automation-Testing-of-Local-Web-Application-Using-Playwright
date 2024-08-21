8166858de76cb2170849e781c69cda0675160d5c
Olha Vadzitska <olgawadzicka@gmail.com>
2024-08-21 15:18:09 +0200

    Add initial Playwright setup and basic tests

---
+++ page-objects/ProductsPage.js
@@ -0,0 +1,47 @@
+import { expect } from "@playwright/test";
+import { Navigation } from "./Navigation.js";
+
+export class ProductsPage {
+  constructor(page) {
+    this.page = page;
+
+    this.addButtons = page.locator('[data-qa="product-button"]');
+    this.basketCounter = page.locator('[data-qa="header-basket-count"]');
+    this.sortDropdown = page.locator('[data-qa="sort-dropdown"]');
+    this.productTitle = page.locator('[data-qa="product-title"]');
+  }
+
+  visit = async () => {
+    await this.page.goto("/");
+  };
+
+  getBasketCount = async () => {
+    await this.basketCounter.waitFor();
+    await this.productTitle.first().waitFor();
+    const productTitleBeforeSorting = await this.productTitle.allInnerText;
+    const text = await this.basketCounter.innerText();
+    return parseInt(text, 10);
+  };
+
+  addProductToBasket = async (index) => {
+    const specificButton = this.addButtons.nth(index);
+    await specificButton.waitFor();
+    await expect(specificButton).toHaveText("Add to Basket");
+    const navigation = new Navigation(this.page);
+    const basketCountBeforeAdding = await this.getBasketCount();
+    await specificButton.click();
+    await expect(specificButton).toHaveText("Remove from Basket");
+    const basketCountAfterAdding = await this.getBasketCount();
+    expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding);
+  };
+
+  sortByCheapest = async () => {
+    await this.sortDropdown.waitFor();
+    await this.productTitle.first().waitFor();
+    const productTitleBeforeSorting = await this.productTitle.allInnerTexts();
+    await this.sortDropdown.selectOption("price-asc");
+    const productTitleAfterSorting = await this.productTitle.allInnerTexts();
+    expect(productTitleAfterSorting).not.toEqual(productTitleBeforeSorting);
+  
+  };
+}
